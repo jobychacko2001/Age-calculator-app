59902947daee583b68131f4c3b40750cd479d0c4
Joby Chacko <jobychacko2001@gmail.com>
2023-05-08 19:58:50 +0530

    script change

--- script.js
+++ script.js
@@ -1,42 +1,43 @@
-// console.log("hello world");
+const form = document.getElementById("my-form");
 
-function Calculate (){
-    const day = document.getElementById("day").value;
-    const month =  document.getElementById("month").value;
-    const year =  document.getElementById("year").value;
+form.addEventListener("submit",function(event){
+    event.preventDefault();
+    const dayInput = document.getElementById("day");
+    const monthInput = document.getElementById("month");
+    const yearInput = document.getElementById("year");
 
-    // console.log("value of day is" + day);
-    // console.log("value of month is" + month);
-    // console.log("value of year is" + year);
+    const day = dayInput.value;
+    const month = monthInput.value;
+    const year = yearInput.value;
 
     //converting to date format
-    var date = new Date(year,month-1,day);
-   
+    var date = new Date(year, month - 1, day);
+
     //  console.log("date entered by user"+date);
-    
+
     //using date constructor to get todays date
     var today = new Date();
-    console.log("todays date"+today);
-    
+    console.log("todays date" + today);
+
     //comparison
     var result_ms = Math.abs(today - date);
-    console.log("difference in ms"+result_ms);
+    console.log("difference in ms" + result_ms);
 
-    var result_days = Math.ceil(result_ms/(1000*60*60*24));
-    console.log("difference in days"+result_days);
+    var result_days = Math.ceil(result_ms / (1000 * 60 * 60 * 24));
+    console.log("difference in days" + result_days);
 
-    var result_years = Math.floor(result_days/365.25);
-    console.log("result_years "+result_years);
+    var result_years = Math.floor(result_days / 365.25);
+    console.log("result_years " + result_years);
 
-    var result_months = Math.floor((result_days%365.25)/30.44);
-    console.log("result_month"+result_months);
+    var result_months = Math.floor((result_days % 365.25) / 30.44);
+    console.log("result_month" + result_months);
 
-    var days_output = Math.floor((result_days%365)%30.44);
-    console.log("result_days "+days_output);
+    var days_output = Math.floor((result_days % 365) % 30.44);
+    console.log("result_days " + days_output);
 
-    document.getElementById("year_res").innerHTML= result_years;
-    document.getElementById("month_res").innerHTML= result_months;
-    document.getElementById("day_res").innerHTML= days_output;
+    document.getElementById("year_res").innerHTML = result_years;
+    document.getElementById("month_res").innerHTML = result_months;
+    document.getElementById("day_res").innerHTML = days_output;
     
-}
+});
 
